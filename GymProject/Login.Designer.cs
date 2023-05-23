1074ba3d460bf9721d1e97016a7a6f033ea12e6e
Leonardo Siqueira <leo2017siqueira@outlook.com>
2023-05-23 11:35:51 -0300

    Ajustando Tela de Login com dados cadastrados do Personal

---
+++ GymProject/Login.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace GymProject
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.UserLogin = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.UserSenha = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Adminlbl = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkCyan;
+            this.panel1.Location = new System.Drawing.Point(4, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(247, 387);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(362, 12);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(206, 156);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 21;
+            this.pictureBox3.TabStop = false;
+            // 
+            // UserLogin
+            // 
+            this.UserLogin.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UserLogin.Location = new System.Drawing.Point(275, 203);
+            this.UserLogin.Name = "UserLogin";
+            this.UserLogin.Size = new System.Drawing.Size(222, 27);
+            this.UserLogin.TabIndex = 48;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(271, 175);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 21);
+            this.label2.TabIndex = 49;
+            this.label2.Text = "User:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(271, 249);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 21);
+            this.label3.TabIndex = 51;
+            this.label3.Text = "Senha:";
+            // 
+            // UserSenha
+            // 
+            this.UserSenha.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UserSenha.Location = new System.Drawing.Point(275, 282);
+            this.UserSenha.Name = "UserSenha";
+            this.UserSenha.Size = new System.Drawing.Size(222, 27);
+            this.UserSenha.TabIndex = 50;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkCyan;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button1.FlatAppearance.BorderSize = 8;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(385, 320);
+            this.button1.Margin = new System.Windows.Forms.Padding(8, 4, 3, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 39);
+            this.button1.TabIndex = 52;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Adminlbl
+            // 
+            this.Adminlbl.AutoSize = true;
+            this.Adminlbl.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Adminlbl.Location = new System.Drawing.Point(436, 368);
+            this.Adminlbl.Name = "Adminlbl";
+            this.Adminlbl.Size = new System.Drawing.Size(49, 17);
+            this.Adminlbl.TabIndex = 53;
+            this.Adminlbl.Text = "Admin";
+            this.Adminlbl.Click += new System.EventHandler(this.Adminlbl_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(672, 397);
+            this.Controls.Add(this.Adminlbl);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.UserSenha);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.UserLogin);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.TextBox UserLogin;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox UserSenha;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label Adminlbl;
+    }
+}
---
+++ GymProject/Login.cs
@@ -0,0 +1,66 @@
+﻿using GymProject.Funcoes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI.WebControls.WebParts;
+using System.Windows.Forms;
+
+namespace GymProject
+{
+    public partial class Login : Form
+    {
+        Func Con;
+        public Login()
+        {
+            InitializeComponent();
+            Con = new Func();
+        }
+        public static int Id;
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Adminlbl_Click(object sender, EventArgs e)
+        {
+            Trainer obj = new Trainer();
+            obj.Show();
+            this.Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (UserLogin.Text == "" || UserSenha.Text == "")
+            {
+                MessageBox.Show("Dados invalidos!!!");
+            } else
+            {
+                try
+                {
+                    string Query = "select * from Personal where PNome = '{0}' and PSenha = '{1}' ";
+                    Query = string.Format(Query, UserLogin.Text, UserSenha.Text); 
+                    DataTable dt = Con.GetData(Query);
+                    if (dt.Rows.Count == 0)
+                    {
+                        MessageBox.Show("Personal Invalido!!");
+                    } else
+                    {
+                        Id = Convert.ToInt32(dt.Rows[0][0].ToString());
+                        Clientes obj = new Clientes();
+                        obj.Show();
+                        this.Hide();
+                    }
+
+                } catch (Exception ex) {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+    }
+}
